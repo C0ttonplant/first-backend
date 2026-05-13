c91bc3db5a6858a6ff508b3475fe53e4eb986c0e
C0ttonplant <thebestofther@gmail.com>
2026-05-13 10:27:04 -0600

    add persistant database

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from database import get_db
 from modles import Idea
+from sqlmodel import Session, select
 from datetime import date
 
 app = FastAPI()
 
-ideas: List[Idea] = []
-
 
 @app.get("/")
 async def hello() -> str:
-    return "hello from Riley Buck"
+    return "<!doctype html><html>hey!</html>"
 
 
 @app.get("/ideas")
-async def get_ideas() -> List[Idea]:
-    return ideas
+async def get_ideas(db: Session = Depends(get_db)):
+    return db.exec(select(Idea)).all()
 
 
 @app.post("/ideas")
-async def create_idea(title: str, content: str):
+async def create_idea(title: str, content: str, db: Session = Depends(get_db)):
     idea = Idea(title=title, content=content, created_at=date.today())
-    ideas.append(idea)
+    db.add(idea)
+    db.commit()
